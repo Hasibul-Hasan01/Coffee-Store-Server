cd5c4610ba0374f4df4ce639cab0e80377b60039
Hasibul-Hasan01 <hasibul.outlook@gmail.com>
2025-11-17 23:20:13 +0600

    Get single coffee by ID function complete

--- index.js
+++ index.js
@@ -38,6 +38,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/coffees/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+
 
     // ADD Coffee On Database
     app.post('/coffees', async (req, res) => {

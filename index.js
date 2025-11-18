8c08377ceccfe73038dc13e1422cf6614f18089b
Hasibul-Hasan01 <hasibul.outlook@gmail.com>
2025-11-18 23:06:36 +0600

    Update Coffee Backend API Complete

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/coffee/:id', async (req, res) => {
+    app.get('/coffees/:id', async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await coffeeCollection.findOne(query);
@@ -57,7 +57,7 @@ async function run() {
     // # Update Coffee
     app.put('/coffees/:id', async (req, res) => {
       const id = req.params.id;
-      const query = { _id: new ObjectIde(id) };
+      const query = { _id: new ObjectId(id) };
       const updateCoffee = req.body;
       const updateDoc = {
         $set : updateCoffee
